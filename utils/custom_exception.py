aaea27860b6b3fe14a7b9dd447a3e3133b93ee47
Owais Ahmad <146345893+codeWithOwaisAhmad@users.noreply.github.com>
2026-03-04 21:49:44 +0500

    Update custom_exception.py

--- utils/custom_exception.py
+++ utils/custom_exception.py
@@ -9,7 +9,7 @@ class CustomException(Exception):
     def get_detailed_error_message(message, error_detail):
         _, _, exc_tb = sys.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename if exc_tb else "Unknown File"
-        line_number = exc_tb.tb_lineno if exc_tb else "Unknown Line"
+        line_number = exc_tb.tb_lineno if exc_tb else "Unknown Line" 
         return f"{message} | Error: {error_detail} | File: {file_name} | Line: {line_number}"
 
     def __str__(self):
